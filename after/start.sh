b03b7a3fefaee0f601f1e37bd774e00d63b9347c
root <root@ip-172-31-20-37.ec2.internal>
2021-11-16 07:08:25 +0000

    builspect

--- after/start.sh
+++
@@ -1,5 +0,0 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 484010831236.dkr.ecr.us-east-1.amazonaws.com
-docker build -t eternals ../
-docker tag eternals:latest 484010831236.dkr.ecr.us-east-1.amazonaws.com/eternals:latest
-docker push 484010831236.dkr.ecr.us-east-1.amazonaws.com/eternals:latest
-
